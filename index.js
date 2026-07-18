182618f8b6ac3525d4dbd6ed423688eee8ae2f9f
Tushar <tpal250001@gmail.com>
2026-07-18 19:16:40 +0530

    fix cors error

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const Orderoutes  = require("./routes/Orderroutes.js")
 const Cartroutes  = require("./routes/Cartroutes.js")
 const cors = require("cors")
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: ["https://ecommerce-frontend-v2-six.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json())
 
 app.use("/api/auth/v1",Authroutes)
